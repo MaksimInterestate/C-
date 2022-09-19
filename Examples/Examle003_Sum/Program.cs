ddfa8c53ebeb9f8eb06362679c2aa11bc10b5801
Max <zenit-max@example.com>
2022-09-19 11:45:34 +0300

    Ex003_sum_new Random()

--- Examples/Examle003_Sum/Program.cs
+++ Examples/Examle003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1,2,3,...9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
